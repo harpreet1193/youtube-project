a2e0e6a47a0d0de33c85250b6f83df8a24142845
Harpreet Singh Arora <harpreetarora.aot93@gmail.com>
2024-03-13 15:15:33 +0530

    path params tutorial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,3 +12,30 @@ async def root():
 @app.post("/")
 async def post():
     return {"message": "Hello from post route"}
+
+
+@app.get("/items")
+async def list_items():
+    return {"message": "List Items Route"}
+
+
+# with Path Param
+@app.get("/items/{item_id}")
+async def get_list_by_id(item_id: int):
+    return {"message": f"List Item {item_id}"}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you're healthy"}
+    if food_name == FoodEnum.fruits:
+        return {"food_name": food_name, "message": "you're still healthy but like sweet things"}
+
+    return {"food_name": food_name, "message": "I like chocolate milk"}
